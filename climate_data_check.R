68b62c41c21c933c8fb918374178a0ac1e0e9b9a
JINGYU LIN <linjingy@deakin.edu.au>
2021-03-01 17:17:14 +1100

    change

--- climate_data_check.R
+++
@@ -1,9 +0,0 @@
-library(data.table)
-list <- list.files(pattern = '^p')
-data_list <- lapply(list,fread)
-df_data <- data.frame(matrix(unlist(data_list),nrow = length(data_list),byrow = TRUE))
-dfdata <- data.frame(t(df_data))
-data <- dfdata[-1,]
-plot(data$X790,ylim=c(0,100))
-
-##merge weather data
