725cc70c46210f80e563299f0b15ecb1173c854c
JINGYU LIN <linjingy@deakin.edu.au>
2021-04-09 16:15:46 +1000

    add

--- read_check_data.R
+++ read_check_data.R
@@ -4,7 +4,7 @@ library(ggmap)
 library(sp)
 library(mapview)
 
-##data cleaning##
+##data cleaning##================================================================================================================
 list <- list.files("E:/Lexi/data_paper3/data for SWAT/data1", pattern="\\.csv$",full.names = TRUE)
 data <- lapply(list, read_csv)
 lalo <- lapply(data, function(x) x%>% select(Longitude, Latitude))
@@ -16,7 +16,7 @@ proj4string(lalo_df) <- CRS("+init=epsg:4326")
 mapview(lalo_df)
 
 
-##read NC data##
+##read NC data##================================================================================================================
 library(ncdf4)
 library(raster)
 library(rasterVis)
@@ -39,7 +39,7 @@ colnames(df_cn_runf) <- df_cn_runf[, 1]
 write.csv(df_runoff, file = "china_runoff_2015.csv")
 
 
-##check climate data##
+##check climate data##=====================================================================================================
 library(data.table)
 list <- list.files(pattern = '^p')
 data_list <- lapply(list,fread)
@@ -49,7 +49,7 @@ data <- dfdata[-1,]
 plot(data$X790,ylim=c(0,100))
 
 
-##merge climate data of global weather data including pcp and tmp##
+##merge climate data of global weather data including pcp and tmp##========================================================
 library(data.table)
 setwd("D:/Jinzhu/linjingy/")
 path <- "D:/Jinzhu/linjingy/"
@@ -60,7 +60,7 @@ first_col <- as.numeric(as.factor(tmp_1[['ID']]))
 tmp_all <- transform(tmp_1,'ID'=c(1:4896))
 write.table(tmp_all,file = "tmp_all.txt",sep = ",", quote = FALSE,row.names = FALSE)
 
-##select streamflow data##
+##select streamflow data##=================================================================================================
 library(dplyr)
 path <- "H:/Documents/paper3_water_china/runoff_china/database/data_table.xlsx"
 streamflow <- readxl::read_xlsx(path)
@@ -70,8 +70,92 @@ stfl_35 <- streamflow %>%
   select(date,discharge)
 write.csv(stfl_35,"q_obs_35.csv",row.names = FALSE)
 
-##
-path <- "C:/Users/linjingy/Downloads/precipitation_0019/precipitation_0019/county_daily.csv"
+##===================================================================================================================
+library(dplyr)
+path <- "E:/Lexi/data_paper3/prec_0019/county_daily.csv"
+path_2 <- "E:/Lexi/data_paper3/prec_0019/lon_lat.txt"
 prec <- readr::read_csv(path)
-  
-  
+colnames(prec)[6] <- 'FIRST_2'
+lon <- readr::read_delim(path_2,delim = ",")  
+study <- lon %>% 
+  filter(26<lat & lat< 35) %>%
+  filter(96<lon & lon< 103) 
+study_yalong <- inner_join(prec,study,by = "FIRST_2")
+ID <- data.frame(ID = c(1:86))
+colnames(study_yalong)[5] <- 'NAME'
+colnames(study_yalong)[7] <- 'DATE'
+colnames(study_yalong)[8] <- 'PRECI'
+study_yalong$PREC <- formattable::digits(study_yalong$PREC,3)
+lat <- data.frame(LAT = c(unique(study_yalong[15])))
+long <- data.frame(LON = c(unique(study_yalong[16])))
+name <- data.frame(NAME = c(unique(study_yalong[5])))
+pcp <- cbind(ID,name,lat,long)
+
+group <- study_yalong %>% 
+  group_by(NAME)%>%
+  mutate(PREC=PRECI*10)%>%
+  select(NAME,DATE,PREC)
+list_ya = split(group,group$NAME)
+
+filename <- unique(group$NAME)
+txtfile = paste(filename,".txt",sep = "") 
+
+nam <- list()
+for (i in 1:86) {
+  nam[[i]] <- paste("E:/Lexi/data_paper3/prec_0019/prec/",txtfile[i],sep="")
+  write.table(list_ya[[i]][[3]],nam[[i]],row.names =FALSE,col.names = 20000101,quote = FALSE)
+}
+
+write.table(pcp_0009,"pcp_0009.txt",row.names = FALSE,quote=FALSE,sep=",")
+
+##===================================================================##
+library(dplyr)
+xl_prec <- read.csv("H:/SWAT/SWAT_China/yalong_river/xinlong.csv") %>%
+  select(date,prec,tempmax,tempmin) %>%
+  filter(!is.na(prec),!is.na(tempmax),!is.na(tempmin))
+write.csv(xl_prec,"H:/SWAT/SWAT_China/yalong_river/xl_prec.csv")
+
+
+##=========read gauge-based data================================================================
+library(data.table)
+library(readr)
+library(dplyr)
+library(lubridate)
+path <- "E:/Lexi/data_paper3/weather/gauge-based weather data China (1960-2020)/data"
+setwd(path)
+mypath <- list.dirs(path, full.names = TRUE, recursive = TRUE)
+files <- sapply(mypath, list.files, full.names = TRUE)
+new_dir <- "data_elements"
+dir.create(new_dir, recursive = TRUE)
+for(file in files) {
+  file.copy(file, new_dir)
+}
+num <- c('13011','12030','12001','13003','11002')
+path1 <- "E:/Lexi/data_paper3/weather/gauge-based weather data China (1960-2020)/data/data_elements"
+setwd(path1)
+mylist <- list.files(path1,pattern='13011')
+pre_list <- lapply(mylist,fread)
+all_pre_list <- bind_rows(pre_list)
+colnames(all_pre_list)[c(1,5,6,7,10)] <- c('st_id','year','month','day','pre')
+pre <- all_pre_list %>% 
+  select('st_id','year','month','day','pre') %>%
+  mutate(pcp=pre) %>%
+  mutate(pcp=ifelse(pre>30000 & pre < 34000 & pre != 32700 & pre !=32766, pre%%1000/10,pcp)) %>%
+  mutate(pcp=ifelse(pre==32700,pre*0,pcp))%>%
+  mutate(pcp=ifelse(pre==32766,pre*0-99,pcp))%>%
+  mutate(pcp=ifelse(pre < 30000,pre/10,pcp))%>%
+  mutate(pcp=round(pcp,3))%>%
+  mutate(date= make_date(year,month,day))%>%
+  select('st_id','date','pcp')
+
+list_pre <- split(pre,pre$st_id)
+pre_filename <- unique(pre$st_id)
+txtfile <- paste('pcp',pre_filename,".txt",sep = "") 
+nam <- list()
+for (i in 1:length(unique(pre$st_id))) {
+    nam[[i]] <- paste("E:/Lexi/data_paper3/weather/gauge-based weather data China (1960-2020)/swat_weather/pcp/",txtfile[i],sep="")
+    write.table(list_pre[[i]][[3]],nam[[i]],row.names =FALSE,col.names = list_pre[[i]][[2]][1],quote = FALSE)
+}
+
+###============================================================
+
